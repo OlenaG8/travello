b1bdf6892dbafd5dddfbe7ad1dbe55c76e255462
Olena <135965492+OlenaG8@users.noreply.github.com>
2025-04-30 13:12:36 +0200

    Creating page skeleton

---
+++ oferty.js
@@ -0,0 +1,11 @@
+export const oferty = [
+    {
+        photo: `images/getting-things-done.jpg`,
+        Date: "Jan 1, 2023",
+        title: "Bug Busters",
+        price: 3999,
+        bestseller: true,
+        preview: `It's time to go through the tangled web of frontend bugs and errors. 
+        Now I'm working on strategies for effective debugging, like using browser developer tools to employing console.log statements.`
+    }
+]
