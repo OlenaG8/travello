6d483c1fbcccb4ad874fec89466a1fdba50975cd
Olena <135965492+OlenaG8@users.noreply.github.com>
2025-05-08 20:47:44 +0200

    Adding basic functionalities

--- oferty.js
+++ oferty.js
@@ -1,11 +1,80 @@
-export const oferty = [
+export const postsArray = [
     {
-        photo: `images/getting-things-done.jpg`,
-        Date: "Jan 1, 2023",
-        title: "Bug Busters",
-        price: 3999,
+        id: 1,
+        date: "14 - 20 Lipca 2025",
+        location: "Hallstatt, Austria",
+        availability: "Wolne miejsca: 1 z 25",
+        price: 2999,
+        rating: 5.0,
         bestseller: true,
-        preview: `It's time to go through the tangled web of frontend bugs and errors. 
-        Now I'm working on strategies for effective debugging, like using browser developer tools to employing console.log statements.`
-    }
+        description: `Wizytówka Austrii. Perła regionu Salzkammergut i jedno z najpiękniejszych miejsc w Europie, 
+        którym zachwycają się turyści z całego świata. Położenie Hallstatt jest wyjątkowo spektakularne – nad 
+        Jeziorem Halsztackim, którego krajobraz przyrodniczo-kulturowy został wpisany na listę UNESCO, w otoczeniu stromych gór, 
+        z których „schodzą” malownicze, drewniane domki.`,
+        coverImg: "Hallstatt.jpg",
+        googleMaps: "https://maps.app.goo.gl/WPqsnXirnbEXsSfL8",
+        favicons: [
+            "fa-solid fa-plane-departure",
+            "fa-solid fa-utensils",
+            "fa-solid fa-hotel"
+        ]
+    },
+    {
+        id: 2,
+        date: "8 - 17 Lipca 2025",
+        location: "Interlaken, Szwajcaria",
+        availability: "Wolne miejsca: 16 z 25",
+        price: 3999,
+        rating: 4.9,
+        bestseller: false,
+        description: `Otoczone potężnymi szczytami i turkusowymi jeziorami, miasto oferuje wszystko – od wspinaczki na skaliste grzbiety, 
+        przez rafting na rwących rzekach, aż po ekscytujące loty paralotnią z widokiem na słynny Jungfrau. Wyrusz na całodniowy 
+        trekking do lodowców, przemierzaj via ferraty z zapierającymi dech panoramami i poczuj wolność, jakiej nie zaznasz nigdzie indziej.`,
+        coverImg: "Interlaken-shipping-port.jpg",
+        googleMaps: "https://maps.app.goo.gl/x1gMXczHLuYjrC4J6",
+        favicons: [
+            "fa-solid fa-plane-departure",
+            "fa-solid fa-utensils",
+            "fa-solid fa-hotel"
+        ]
+    },
+    {
+        id: 3,
+        date: "6 - 13 Czerwca 2025",
+        location: "Innsbruck, Austria",
+        availability: "Wolne miejsca: 8 z 16",
+        price: 3599,
+        rating: 4.8,
+        bestseller: true,
+        description: `Innsbruck – alpejskie centrum sportu i adrenaliny! 
+        Tu szczyty zaczynają się tuż za rogiem, a każda ulica prowadzi ku nowej przygodzie. Wjedź kolejką Nordkette prosto 
+        z centrum miasta i w kilka minut znajdź się wśród skalistych turni. Skacz z klifów, śmigaj na rowerze po górskich trasach, 
+        szybuj nad dolinami na paralotni.`,
+        coverImg: "Innsbruck.jpg",
+        googleMaps: "https://maps.app.goo.gl/qEYtRR4EWnPeJbNt5",
+        favicons: [
+            "fa-solid fa-plane-departure",
+            "fa-solid fa-utensils",
+            "fa-solid fa-hotel"
+        ]
+    },
+    {
+        id: 4,
+        date: "12 - 15 Lipca 2025",
+        location: "Cortina d’Ampezzo, Włochy",
+        availability: "Wolne miejsca: 15 z 20",
+        price: 2500,
+        rating: 5.0,
+        bestseller: false,
+        description: `Cortina d’Ampezzo to idealne miejsce dla tych, którzy chcą przekroczyć granice codzienności i poczuć moc Dolomitów. 
+        Czekają tu na Ciebie pionowe ściany, ekscytujące via ferraty, ekstremalne trasy rowerowe i panoramy, które zapierają dech w piersiach. 
+        Rano możesz wspinać się po skalnych wieżach, a wieczorem zrelaksować się przy włoskiej kuchni z widokiem na zachód słońca nad Tofaną.`,
+        coverImg: "Dolomites.jpg",
+        googleMaps: "https://maps.app.goo.gl/y3SCCVpaGohjJ6qZ9",
+        favicons: [
+            "fa-solid fa-plane-departure",
+            "fa-solid fa-utensils",
+            "fa-solid fa-hotel"
+        ]
+    },
 ]
